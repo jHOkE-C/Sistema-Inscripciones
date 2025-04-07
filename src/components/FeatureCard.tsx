0fa3888fed262ced374a26609cb724a8ce282674
Jhunior Garcia <jhuniorremberto1@gmail.com>
2025-04-06 23:26:44 -0400

    correcion cards pagina principal

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,21 +1,32 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "./ui/card";
 
 interface FeatureCardProps {
-  icon: React.ReactNode;
-  title: string;
-  description: string;
+    icon: React.ReactNode;
+    title: string;
+    description: string;
 }
 
 export function FeatureCard({ icon, title, description }: FeatureCardProps) {
-  return (
-    <Card>
-      <CardHeader className="pb-2">
-        {icon}
-        <CardTitle className="mt-2">{title}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <CardDescription>{description}</CardDescription>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card>
+            <CardHeader className="pb-2">
+                <CardTitle className="mt-2 flex flex-col justify-center items-center gap-2">
+            <div className="flex items-center justify-center">
+                    {icon}
+            </div>
+                    {title}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <CardDescription>
+                    {description}</CardDescription>
+            </CardContent>
+        </Card>
+    );
 }
