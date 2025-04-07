c8a30f061a08eb0b2e748fac3abf6dad38da9257
Jhunior Garcia <jhuniorremberto1@gmail.com>
2025-04-06 23:02:28 -0400

    bug importacion

--- src/pages/inscribir/FormCI.tsx
+++ src/pages/inscribir/FormCI.tsx
@@ -16,7 +16,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { z } from "zod";
