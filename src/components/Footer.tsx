6eb8920decf96b9e5d3eb2045f3148aae689fe45
Marko <marcosvelasquezvela123@gmail.com>
2025-04-18 10:13:11 -0400

    mejoras visuales del admin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,12 @@ import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full border-t py-6 md:py-0">
+    <footer className="w-full border-t py-6 md:py-0 mt-auto">
       <div className="px-4 flex flex-col md:flex-row items-center justify-between gap-4 md:h-16">
-        <p className="text-sm text-gray-500">
-          © 2025 Olimpiadas O!Sansi. Todos los derechos reservados.
+        <p className="text-sm text-gray-500 flex gap-1">
+          © 2025 Olimpiadas OhSanSi. <span className='hidden sm:block'>Todos los derechos reservados.</span>
         </p>
+
         <div className="flex gap-4">
           <Link
             to="/terminos"
--- src/pages/admin/page.tsx
+++ src/pages/admin/page.tsx
@@ -37,19 +37,23 @@ const Admin = () => {
     }, []);
 
     return (
-        <>
-            <div className="w-full p-4 md:w-4/5 mx-auto ">
-                <h1 className="text-4xl font-bold text-center py-4">
-                    Panel de Administración
-                </h1>
-               <Botones getData={() => getData()}/>
-                <h2 className="text-2xl font-bold text-center py-4">
-                    Versiones de la Olimpiada
-                </h2>
-                <Versiones versiones={versiones} />
-            </div>
-                <Footer />
-        </>
+      <div className="flex flex-col min-h-screen">
+        <div className="w-full p-4 md:w-4/5 mx-auto">
+          <h1 className="text-4xl font-bold text-center py-4">
+            Panel de Administración
+          </h1>
+          <Botones getData={() => getData()} />
+          <h2 className="text-2xl font-bold text-center py-4">
+            Versiones de la Olimpiada
+          </h2>
+          {versiones.length > 0 ? (
+            <Versiones versiones={versiones} />
+          ) : (
+            <p className="text-center text-gray-500">No hay versiones disponibles aún.</p>
+          )}
+        </div>
+        <Footer />
+      </div>
     );
 };
 
