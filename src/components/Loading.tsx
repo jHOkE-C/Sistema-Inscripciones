b22ae90e7e6ef3ce28a5c1d1a553772bd43a1e78
Jhunior <jhuniorremberto1@gmail.com>
2025-04-23 02:23:46 -0400

    sin fondo

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
     return (
-        <div className="flex min-h-screen w-full items-center justify-center bg-primary/10 flex-col space-y-3">
+        <div className="flex min-h-screen w-full items-center justify-center  flex-col space-y-3">
          
             <Loader2 className="h-16 w-16 animate-spin text-primary" />
             <h1 className="text-3xl font-bold text-primary">Cargando...</h1>
