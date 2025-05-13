6fe74853778194dbb86a9e283f5812a6c15d948c
Jhunior Garcia <jhuniorremberto1@gmail.com>
2025-05-12 20:30:25 -0400

    areas categorias excel opcionales

--- src/utils/excel.ts
+++ src/utils/excel.ts
@@ -52,7 +52,7 @@ export const generarExcel = async (
             { name: "Correo pertenece a" },
         ];
         for (let index = 1; index <= olimpiada.limite_inscripciones; index++) {
-            columns = [...columns, { name: "Área categoría " + index }];
+            columns = [...columns, { name: "Área categoría " + index+ (index>1? "(opcional)" :"") }];
         }
 
         ws.getColumn(4).numFmt = "dd/mm/yyyy";
@@ -192,7 +192,7 @@ export const generarExcel = async (
             }
         }
         for (let i = 1; i <= columns.length; i++) {
-            ws.getColumn(colLetter(i)).width = 20;
+            ws.getColumn(colLetter(i)).width = 30;
         }
     };
 
