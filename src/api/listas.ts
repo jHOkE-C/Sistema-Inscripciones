aeaf68013e37d39689c54cefcfb6fb5a738fda67
jHOkE <jhonumss@gmail.com>
2025-05-26 22:57:23 -0400

    fix put A PATCH

--- src/api/listas.ts
+++ src/api/listas.ts
@@ -15,9 +15,16 @@ export const cambiarEstadoLista = async (
     });
 };
 export const pagarLista = async (codigo: string, n_orden: string, fecha: string, ordenPago: string) => {
+
+    const fechaNormalizada = normalizarFecha(fecha);
     return await request(`/api/ordenes-pago/pagar`, {
-        method: "PUT",
+        method: "PATCH",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ recibo_caja: n_orden, fecha, codigo_lista: codigo, orden_pago: ordenPago }),
+        body: JSON.stringify({ recibo_caja: n_orden, fecha:fechaNormalizada, codigo_lista: codigo, n_orden_pago: ordenPago }),
     });
+};
+
+const normalizarFecha = (fecha: string): string => {
+  const [dd, mm, yyyy] = fecha.split("-");
+  return `${yyyy}-${mm.padStart(2, "0")}-${dd.padStart(2, "0")}`;
 };
