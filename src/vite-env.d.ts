d69cf526f31c9b4f3f3dfe1fbaf40e73d46d2c67
jHOkE <jhonumss@gmail.com>
2025-05-27 22:27:07 -0400

    sin cache

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,16 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference lib="webworker" />
 
-declare const self: ServiceWorkerGlobalScope;
-
-declare module 'virtual:pwa-register' {
-  export interface RegisterSWOptions {
-    immediate?: boolean;
-    onNeedRefresh?: () => void;
-    onOfflineReady?: () => void;
-    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
-    onRegisterError?: (error: Error) => void;
-  }
-
-  export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import compression from "vite-plugin-compression";
-import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,16 +14,7 @@ export default defineConfig({
                 algorithm: 'gzip',
                 ext: '.gz',
                 deleteOriginFile: false,
-            }),
-            VitePWA({
-                strategies: 'injectManifest',
-                srcDir: 'src',
-                filename: 'sw.ts',
-                injectRegister: 'auto',
-                injectManifest: {
-                    injectionPoint: undefined
-                }
-            }),
+            })
 
     ],
     resolve: {
