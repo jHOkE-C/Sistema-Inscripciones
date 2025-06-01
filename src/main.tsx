15b1a042394d6aca661f48f02bd0652492224586
jHOkE <jhonumss@gmail.com>
2025-05-31 23:15:15 -0400

    quitando devtools de tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AppProviders } from "./providers/AppProviders.tsx";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProviders>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </AppProviders>
   </React.StrictMode>
 );
