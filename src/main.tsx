b60f519f936a0ff0566e34c50fb07b5027407650
jhon <jhonumss@gmail.com>
2025-06-08 22:18:16 -0400

    uno

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
+// Antes (no existe default export en 'react-dom/client'):
 import ReactDOM from "react-dom/client";
+
+// Mejor así:
+import { createRoot } from "react-dom/client";
+
 import App from "./App.tsx";
 import "./index.css";
 import { AppProviders } from "./providers/AppProviders.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  
+const container = document.getElementById("root");
+if (!container) throw new Error("No se encontró el elemento #root");
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
     <AppProviders>
       <App />
     </AppProviders>
-  
+  </React.StrictMode>
 );
